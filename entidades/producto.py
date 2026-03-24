f76edca6b0c76e5c4b37f1a8fceb31e1baca7ac7
ernestitoc900-cyber <ernestitoc900@gmail.com>
2026-03-24 11:05:20 -0700

    Metodo clase Producto

--- entidades/producto.py
+++ entidades/producto.py
@@ -1,5 +1,11 @@
+from enums.tipoproducto import TipoProducto 
+
 class Producto:
     def __init__(self, nombre: str, precio: float, cantidad: int, tipo: TipoProducto):
         self.nombre = nombre
         self.precio = precio
         self.cantidad = cantidad
+        self.tipo = tipo 
+
+    def mostrar(self):
+        return f"Producto(nombre: {self.nombre}, precio: {self.precio}, cantidad: {self.cantidad}, tipo: {self.tipo})"
