4d2f070bcadd1c1b146b856074e3f127de50729f
ernestitoc900-cyber <ernestitoc900@gmail.com>
2026-03-24 10:40:07 -0700

    Clase padre

---
+++ entidades/producto.py
@@ -0,0 +1,5 @@
+class Producto:
+    def __init__(self, nombre: str, precio: float, cantidad: int, tipo: TipoProducto):
+        self.nombre = nombre
+        self.precio = precio
+        self.cantidad = cantidad
